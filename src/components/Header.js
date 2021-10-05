4b8f5e92fa6c9f02eafc2de3311a337add07899e
herman connor <hermanconnor11@gmail.com>
2021-10-04 22:07:31 -0400

    Create global styles component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import StyledHeader from './styles/Header.styled';
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <h1>Yo</h1>
+    </StyledHeader>
+  );
+};
+
+export default Header;
